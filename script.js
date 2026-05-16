1199679ca95f232fd779a5bd6b69ea2862b4f174
Jonathan Henriquez <jstronghold2016@gmail.com>
2026-05-15 21:30:25 -0500

    feat: verificar conexion

---
+++ script.js
@@ -0,0 +1,6 @@
+const selectorTipo = document.getElementById("tipoConversion");//selector del tipo de conversion
+const unidadOrigen = document.getElementById("unidadOrigen");//selector de la unidad de origen
+const unidadDestino = document.getElementById("unidadDestino");//selector de la unidad de destino
+
+selectorTipo.addEventListener("change", function() {
+    console.log("El usuario ha seleccionado el tipo de conversión: " );});
